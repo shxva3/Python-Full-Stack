5abad25655ed73e8c254e925cdaff8607df76f31
SHIVA <shiva98492@gmail.com>
2026-07-04 13:12:22 +0530

    Day 11(coding practice)
    Python Practice – Logic Building & Pattern Programs
    
    This repository contains a collection of Python practice programs focused on strengthening programming fundamentals and logical thinking.
    
    Topics Covered
    Prime Number Checker
    Armstrong Number Checker
    Perfect Number Checker
    Fibonacci Series
    Multiplication Table Generator
    Star & Number Pattern Programs
    Simple Calculator
    Loops and Conditional Statements

--- coding.py
+++ coding.py
@@ -42,15 +42,15 @@ Enter a number:4
 7 8 9 10 
 ------------------------------------
 
-am_str=int(input("Enter a number: "))          # eg:153=1^3+5^3+3^3=153
-length=len(str(am_str))
-all_sum=0
-for j in str(am_str):
-    all_sum+=int(j)**length
-if all_sum==am_str:
-    print(f"{am_str} is an amstrong number")
+a=int(input("Enter a number: "))          # eg:153=1^3+5^3+3^3=153
+b=len(str(am_str))
+c=0
+for j in str(a):
+    c+=int(j)**b
+if c==a:
+    print(f"{a} is an amstrong number")
 else:
-    print(f"{am_str} is not an amstrong number")
+    print(f"{a} is not an amstrong number")
 
 o/p:
 Enter a number: 153
@@ -75,5 +75,103 @@ o/p:
   ***************
  *****************
  ----------------------------------------
- ,,,
-*******************
+
+a=int(input("Enter a number: "))          # eg:153=1^3+5^3+3^3=153
+b=len(str(a))
+c=0         #count
+for j in str(a):
+    c+=int(j)**b
+if c==a:
+    print(f"{a} is an amstrong number")
+else:
+    print(f"{a} is not an amstrong number")
+
+---------------------------------------------
+
+num=int(input("Enter a number: "))
+for i in range(1,num+1):
+    print("* "*(i))
+
+---------------------------------
+
+num=int(input("Enter a number: "))
+for j in range(num):
+    print(" "*(num-j-1),end="*")
+    for i in range(j+1):
+        print(i,end="*")
+ ------------------------------
+ 
+n=int(input())
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print(i-j,end="")
+    print()
+ -----------------------------
+so=input("Enter a word: ")
+empty= ""
+for j in so:
+    empty=j+empty
+    print(
+
+-------------------------------
+num=0
+num_2=1
+limit=int(input())
+print(num,num_2,end=" ")
+for i in range(1,limit+1):
+    all=num+num_2
+    num=num_2
+    num_2=all
+    print(all,end=" ")
+-------------------------------
+calculator 
+val=int(input("Enter a number"))
+val2=int(input("Enter a number"))
+user_in=int(input("enter \n1.add \n2.sub \n3.mul \n4.pow: "))
+if user_in ==1:
+    print(val+val2)
+elif user_in ==2:
+    print(val-val2)
+elif user_in ==3:
+    print(val*val2)
+else:
+    print(val**val2)
+
+--------------------------------
+table=int(input("Enter a number: "))
+for val in range(1,13):
+    print(f"{table} X {val} = {table*val}")
+--------------------------------
+'''
+num=int(input("Enter a number:"))
+sum_=0
+for j in range(1,num):
+    if num%j==0:
+        sum_+=j
+if sum_==num:
+    print(f"{num} is a perfect num")
+else:
+    print(f"{num} is not a perfect num")
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
