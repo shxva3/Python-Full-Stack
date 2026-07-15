215f0776cf0b4f1dda2815b81c56c17183a94da4
SHIVA <shiva98492@gmail.com>
2026-07-15 23:54:26 +0530

    Day-20
    Started Object-Oriented Programming in Python with classes and objects.

---
+++ day-20 oops (1).py
@@ -0,0 +1,29 @@
+"""
+OOPS
+====
+object oriented programming system (oops),this will be organize the code using classes and objects..
+
+use
+---
+code reusable
+easy maintainence
+clear understanding
+better security
+
+classes
+--------
+class is a blueprint or a template used to create an object...
+
+class batch_4:
+    pass
+
+object
+-------
+
+object is a instance of the class
+"""
+
+class shiva:
+    a="rocky"
+rocky=shiva()
+print(rocky)
