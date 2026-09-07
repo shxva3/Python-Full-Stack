13b4706c3613905366691b3e7e3ff3f49daf744b
shxva3 <shxva3@gmail.com>
2026-09-07 13:16:38 +0530

    RecapofDatatypes

---
+++ DSA-1.py
@@ -0,0 +1,12 @@
+'''email_id="saketh@codegnan.com"
+print(email_id[7:15])'''
+email_id=['shiva@gmail.com','gopi@gmail.com','sai@gmail.com']
+#print(len(email_id))
+#print(email_id[-2])
+#email_id.extend([])
+users=dict.fromkeys(email_id)
+'''print(users)
+users['shiva@gmail.com']=2,3'''
+for i in range(len(email_id)):
+    users[i+1]=email_id[i]
+print(users)
