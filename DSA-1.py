64407771ee91247a45a839978ac38e336f4daf5c
SHIVA <shiva98492@gmail.com>
2026-09-08 13:17:31 +0530

    Refactor email_id handling and user dictionary creation

--- DSA-1.py
+++ DSA-1.py
@@ -1,13 +1,10 @@
-'''email_id="saketh@codegnan.com"
-print(email_id[7:15])'''
-email_id=['shiva@gmail.com','gopi@gmail.com','sai@gmail.com']
-#print(len(email_id))
-#print(email_id[-2])
-#email_id.extend([])
-'''users=dict.fromkeys(email_id)
+email_id = ['shiva@gmail.com', 'gopi@gmail.com', 'sai@gmail.com']
+print(len(email_id))
+print(email_id[-2])
+users = dict.fromkeys(email_id)
 print(users)
-users['shiva@gmail.com']=2,3'''
+users['shiva@gmail.com'] = (2, 3)
 print(users)
-fori in range(len(email_id)):
-    users[i+1]=email_id[i]
-[
+for i in range(len(email_id)):
+    users[i + 1] = email_id[i]
+print(users)
